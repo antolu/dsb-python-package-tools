3a55cbb843ae01bd7edf06683f4306665214bede
Anton Lu <anton.lu@cern.ch>
2026-04-17 22:55:42 +0200

    feat(maintenance): register SetupPyStep

--- dsb_devtools/maintenance/_steps.py
+++ dsb_devtools/maintenance/_steps.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from ._types import MaintenanceStep
 from .steps.base_dist import BaseDistStep
+from .steps.setup_py import SetupPyStep
 
 STEPS: list[MaintenanceStep] = [
     BaseDistStep(),
+    SetupPyStep(),
 ]
